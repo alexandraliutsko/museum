bb0712e2e99b2a0f222568772997707c0ae43477
Nekoguard <nekoguard@protonmail.com>
2021-12-13 11:40:46 +0300

    AL/add welcome-section layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
+import * as $ from 'jquery';
+import * as slider from 'slick-carousel';
+import changeProgress from "./player";
 import '../styles/style.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
